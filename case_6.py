74885798740deda93e5e276cd35542abd232a4f2
sparkinson <samuel.d.parkinson@googlemail.com>
2012-05-13 23:31:45 +0100

    added some cases

---
+++ case_6.py
@@ -0,0 +1,29 @@
+#! /usr/bin/env python
+
+from dolfin import *
+from numpy import *
+import distribution_diffusion as mod
+
+def main():
+
+    # some constants
+    T = 5.0
+    dt = 0.1
+    dim = 1
+    n_ele = 40
+    N = 2
+    A_cond = 1e-11
+    A_pert = 5e-4
+    D_x = 1e-3
+    G = 1.0
+    minField = 1e-10
+    abscissa_0 = [Expression('0.2'),
+                  Expression('-0.2')]
+    weight_0 = [Expression('0.6*floor(x[0]+0.5) + 0.4*(1.0-floor(x[0]+0.5))'), 
+                Expression('0.4*floor(x[0]+0.5) + 0.6*(1.0-floor(x[0]+0.5))')]
+
+    # import pdb; pdb.set_trace()
+
+    mod.main(T, dt, dim, n_ele, N, A_cond, A_pert, D_x, abscissa_0, weight_0)
+
+main()
---
+++ case_8.py
@@ -0,0 +1,28 @@
+#! /usr/bin/env python
+
+from dolfin import *
+from numpy import *
+import distribution_diffusion as mod
+
+def main():
+
+    # some constants
+    T = 5.0
+    dt = 0.1
+    dim = 2
+    n_ele = 41
+    N = 2
+    A_cond = 1e-11
+    A_pert = 5e-4
+    D_x = 1e-2
+    minField = 1e-10
+    abscissa_0 = [Expression('0.6*floor(x[0]+0.5) + 0.4*(1.0-floor(x[0]+0.5))'),
+                  Expression('0.4*floor(x[0]+0.5) + 0.2*(1.0-floor(x[0]+0.5))')]
+    weight_0 = [Expression('0.25*floor(x[0]+0.5) + 0.25*(1.0-floor(x[0]+0.5))'), 
+                Expression('0.25*floor(x[0]+0.5) + 0.25*(1.0-floor(x[0]+0.5))')]
+
+    # import pdb; pdb.set_trace()
+
+    mod.main(T, dt, dim, n_ele, N, A_cond, A_pert, D_x, abscissa_0, weight_0)
+
+main()
